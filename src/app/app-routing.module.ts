170aef6a3fd2f693cd5d25a0db2c5d01d99ff9dc
Fabi <fabianaasara@Fabianas-MacBook-Pro.local>
2021-08-07 13:37:15 +0100

    routing is working nicely!

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { ComponentOneComponent } from './component-one/component-one.component';
+import { ComponentTwoComponent } from './component-two/component-two.component';
+import { ComponentThreeComponent } from './component-three/component-three.component';
+import { HomeComponent } from './home/home.component';
+
+
+const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: 'component-one', component: ComponentOneComponent },
+  { path: 'component-two', component: ComponentTwoComponent },
+  { path: 'component-three', component: ComponentThreeComponent },
+  { path: '**', component: HomeComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { ComponentOneComponent } from './component-one/component-one.component';
 import { ComponentTwoComponent } from './component-two/component-two.component';
 import { ComponentThreeComponent } from './component-three/component-three.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ComponentOneComponent,
     ComponentTwoComponent,
-    ComponentThreeComponent
+    ComponentThreeComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
